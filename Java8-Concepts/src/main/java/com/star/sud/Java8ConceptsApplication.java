10fe55ea8a2b92bf7f2b2f6c1f1091503a0cfff9
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2021-05-25 02:56:09 +0530

    Project-Setup

---
+++ Java8-Concepts/src/main/java/com/star/sud/Java8ConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.star.sud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ConceptsApplication.class, args);
+	}
+
+}
